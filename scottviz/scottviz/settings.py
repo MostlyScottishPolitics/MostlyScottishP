ad2b0c58821ad72b84423621f1c12fb7fea6cbaf
Cristinapisi <2168879m@student.gla.ac.uk>
2014-10-21 18:46:25 +0100

    database models done, but cannot syncdb  error:  OperationalError: could not connect to server: Connection refused (0x0000274D/10061) 	Is the server running on host "127.0.0.1" and accepting 	TCP/IP connections on port 5432?

--- scottviz/scottviz/settings.py
+++ scottviz/scottviz/settings.py
@@ -27,8 +27,8 @@ DATABASES = {
         # The following settings are not used with sqlite3:
         'USER': '',
         'PASSWORD': '',
-        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '',                      # Set to empty string for default.
+        'HOST': '127.0.0.1',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'PORT': '5432',                      # Set to empty string for default.
     }
 }
 
--- scottviz/scottviz_app/models.py
+++ scottviz/scottviz_app/models.py
@@ -2,24 +2,56 @@ from django.db import models
 from datetime import datetime
 
 
-class Party(models.Model):
-	name = models.CharField(max_length=128, unique=True)	
+class SPsession(models.Model):
+	msps = models.ManyToManyField(MSP)
+	session = models.IntegerField()
+	startdate = models.DateField()
+	enddate = models.DateField()
 	def __unicode__(self):
 		return self.name
 
 class Constituency(models.Model):
-	parent = models.ForeignKey('self', default=0)
+	msps = models.ManyToManyField(MSP)
 	name = models.CharField(max_length=128, unique=True)
-
+	Type = models.BooleanField()
+	regionname = models.CharField(max_length=128)
 	def __unicode__(self):
 		return self.name
 	
 class MSP(models.Model):
-	foreignid = models.PositiveIntegerField(max_length=8, unique=True)
-	constituency = models.ForeignKey(Constituency)
+	spsession = models.ManyToManyField(SPsession)
+	constituency = models.ManyToManyField(Constituency)
+	divisions = models.ForeignKey(Division,unique=True)
+	votes = models.ForeignKey(Vote,unique=True)
 	party = models.ForeignKey(Party)
 	firstname = models.CharField(max_length=128)
 	lastname = models.CharField(max_length=128)
+	job = models.CharField(max_length=128)
+	def __unicode__(self):
+		return self.name
 
+class Party(models.Model):
+	name = models.CharField(max_length=128)
+	def __unicode__(self):
+		return self.name
+
+class Vote(models.Model):
+	msps = models.ForeignKey(MSP,unique=True)
+	divisions = models.ForeignKey(Division,unique=True)
+	type = models.CharField(max_length=128)
+	def __unicode__(self):
+		return self.name
+
+class Division(models.Model):
+	msps = models.ForeignKey(MSP,unique=True)
+	votes = models.ForeignKey(Vote,unique=True)
+	date = models.DateField()
+	number = models.IntegerField()
+	link = models.URLField(max_length=128)
+	agreement = models.BooleanField()
+	question = models.TextField()
+	motionid = models.CharField(max_length=12)
+	motiontext = models.TextField()
+	topic = models.CharField(max_length=30)
 	def __unicode__(self):
 		return self.name
