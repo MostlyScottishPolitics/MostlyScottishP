84948f9db475c030502fa809f2f2be2741f20f9c
Laura Voinea <1006414v@student.glasgow.ac.uk>
2014-11-15 20:55:18 +0000

    votes+divisions

--- scottviz/populate_test_data.py
+++ scottviz/populate_test_data.py
@@ -1,4 +1,7 @@
+import csv
 import os
+import datetime
+
 from scottviz.settings import STATIC_PATH
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "scottviz.settings")
@@ -15,7 +18,7 @@ def delete_data():
 
 
 def populate_party():
-    with open(STATIC_PATH+'/test_data/parties.csv') as f:
+    with open(STATIC_PATH + '/test_data/parties.csv') as f:
         next(f)
         for line in f:
             line = line.split(',')
@@ -24,7 +27,7 @@ def populate_party():
 
 
 def populate_constituency():
-    with open(STATIC_PATH+'/test_data/districts.csv') as f:
+    with open(STATIC_PATH + '/test_data/districts.csv') as f:
         next(f)
         for line in f:
             line = line.split(',')
@@ -43,26 +46,27 @@ def populate_constituency():
 
 
 def populate_msps():
-    with open(STATIC_PATH+'/test_data/msps.csv') as f:
+    with open(STATIC_PATH + '/test_data/msps.csv') as f:
         next(f)
         for row in f:
             row = row.split(',')
             p = Party.objects.get(id=int(float(row[3])))
             fid = int(float(row[1]))
             c = Constituency.objects.get(id=int(float(row[2])))
-            m = MSP(id=int(float(row[0])),firstname=row[4], lastname=row[5], constituency=c, party=p, foreignid=fid)
+            m = MSP(id=int(float(row[0])), firstname=row[4], lastname=row[5], constituency=c, party=p, foreignid=fid,
+                    img=row[len(row) - 1].strip())
             m.save()
 
 
 def populate_votes():
-    with open(STATIC_PATH+'/test_data/votes.csv') as f:
+    with open(STATIC_PATH + '/test_data/votes.csv') as f:
         next(f)
         for line in f:
             line = line.split(',')
-            print line
             id = int(float(line[0]))
             msp = MSP.objects.get(id=int(float(line[1])))
             division = Division.objects.get(id=int(float(line[2])))
+            v = None
             if line[3] == '0':
                 v = models.Vote.ABSENT
             elif line[3] == '1':
@@ -71,41 +75,49 @@ def populate_votes():
                 v = models.Vote.YES
             elif line[3] == '3':
                 v = models.Vote.NO
-            vote = Vote(id=id, msp=msp, division=division, votes=v)
+            vote = Vote.objects.get_or_create(id=id, msp=msp, division=division, vote=v)[0]
+            print vote.msp
             vote.save()
 
 
 def populate_divisions():
-    with open(STATIC_PATH+'/test_data/divisions.csv') as f:
-        next(f)
-        for row in f:
-            row = row.split(',')
-            id = int(float(row[0]))
-            parent = int(float(row[1]))
-            print parent
-            identifier = row[2]
-            text = row[5]
-            result = row[6]
-            if row[6] == '0':
-                result = models.Division.CARRIED
-            elif row[6] == '1':
-                result = models.Division.DEFEATED
-            else:
-                pass
-
-            if parent == 0:
-                d = Division(id=id, parent=None, motiontext=text, motionid=identifier, result=result)
-                d.save()
-            else:
-                pere = Division.objects.get(id=parent)
-                d = Division(id=id, parent=pere, motiontext=text, motionid=identifier, result=result)
-                d.save()
+    f = open(STATIC_PATH + '/test_data/divisions.csv')
+    csv_f = csv.reader(f)
+    next(csv_f)
+    for row in csv_f:
+        id = int(float(row[0]))
+        parent = int(float(row[1]))
+        identifier = row[2].strip()
+        text = row[5]
+        result = row[len(row) - 2]
+        if len(identifier)>12:
+            identifier = 'missing'
+        if row[len(row) - 2] == '1':
+            result = Division.DEFEATED
+        elif row[len(row) - 2] == '2':
+            result = Division.CARRIED
+        if parent == 0:
+            d = Division(id=id, parent=None, date=datetime.date.today(), motionid=identifier, result=result)
+            d.save()
+        else:
+            pere = Division.objects.get(id=parent)
+            d = Division(id=id, parent=pere, motiontext=text, date=datetime.date.today(), motionid=identifier,
+                         result=result)
+            d.save()
 
 
 if __name__ == '__main__':
     delete_data()
+    print "__done__delete"
     populate_party()
+    print "__done__party"
+
     populate_constituency()
+    print "__done__reg"
+
     populate_msps()
+    print "__done__msp"
     populate_divisions()
+    print "__done__div"
     populate_votes()
+    print "__done!__"
