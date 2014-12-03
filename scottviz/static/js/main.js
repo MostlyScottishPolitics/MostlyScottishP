53b7cf314da3c6f0f5bb4aebc8139deded08491d
LauraVoinea <1006414v@student.glasgow.ac.uk>
2014-12-03 18:10:30 +0000

    constituencies in regions

--- scottviz/static/js/main.js
+++ scottviz/static/js/main.js
@@ -1,5 +1,12 @@
-$('#top').affix({
-      offset: {
-        top: $('header').height()
-      }
+$('#constituencies a').click(function (e) {
+    e.preventDefault()
+    $(this).tab('show')
+    $("#rc").hide();
+    $("#cc").show();
+});
+$('#regions a').click(function (e) {
+    e.preventDefault()
+    $(this).tab('show')
+    $("#cc").hide();
+    $("#rc").show();
 });
