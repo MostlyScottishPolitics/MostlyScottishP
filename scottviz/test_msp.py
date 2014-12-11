0a93a1a0b6f01e652f0953e4a817712828265e7a
LauraVoinea <1006414v@student.glasgow.ac.uk>
2014-12-11 01:42:32 +0000

    starting unit tests for msp

---
+++ scottviz/test_msp.py
@@ -0,0 +1,16 @@
+__author__ = 'laura'
+
+from django.test import TestCase
+from msp.models import *
+
+
+class PartyTestCase(TestCase):
+    def setUp(self):
+        Party.objects.create(id=1, name="party")
+        Party.objects.create(id=1, name="politics", description="greatest party ofc")
+
+    def test_party(self):
+        p1 = Party.objects.get(id=1)
+        p2 = Party.objects.get(id=2)
+        self.assertEqual(p1.name, 'party')
+        self.assertEqual(p2.category, 'politics')
