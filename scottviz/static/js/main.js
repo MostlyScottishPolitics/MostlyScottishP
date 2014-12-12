de11093fc77126e314aa5874117410feff925dbe
LauraVoinea <1006414v@student.glasgow.ac.uk>
2014-12-12 14:47:23 +0000

    dropdown glitch fixed

--- scottviz/static/js/main.js
+++ scottviz/static/js/main.js
@@ -55,10 +55,11 @@ $('#related a').click(function (e) {
     $("#relc").show();
 });
 
-$('.dropdown').hover(function(){
-  $('.dropdown-toggle', this).trigger('click');
+jQuery('ul.nav li.dropdown').hover(function() {
+  jQuery(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn();
+}, function() {
+  jQuery(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut();
 });
-
 $(function () {
   $('[data-toggle="tooltip"]').tooltip()
 })
