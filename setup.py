ffd3a416f4f0a5884d9be50e8345dd26bff147f5
LauraVoinea <1006414v@student.glasgow.ac.uk>
2014-12-12 18:47:03 +0000

    our very own setup script

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+# setup script
+setup(
+    name='msp',
+    version='0.8',
+    description='Mostly Scottish Politics',
+    author='Team C',
+    packages=find_packages(),
+    include_package_data=True,
+)
