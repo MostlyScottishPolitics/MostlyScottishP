a114dcb3308483c82b44e76c74e0370f6172613c
LauraVoinea <1006414v@student.glasgow.ac.uk>
2014-12-12 22:13:52 +0000

    ?

--- scottviz/hitcounter/models.py
+++ scottviz/hitcounter/models.py
@@ -8,12 +8,11 @@ class Hit(models.Model):
     hit model
     """
     time = models.DateTimeField(auto_now_add=True, editable=False)
-    ipAddress = models.IPAddressField()
+    ipAddress = models.IPAddressField(null=True)
     session = models.CharField(max_length=40, null=True)
     url = models.CharField(_('URL'), max_length=2000)
 
     def save(self, *args, **kwargs):
-        print self.ipAddress
         # save only if the required amount of time has passed since last visit
         if self.id:
             hits = Hit.objects.filter(ipAddress=self.ipAddress)
@@ -25,12 +24,5 @@ class Hit(models.Model):
                 super(Hit, self).save(*args, **kwargs)
         else:
             super(Hit, self).save(*args, **kwargs)
-
-    def check_expiration(self):
-        now = datetime.now()
-
-        if self.time - timedelta(minutes=int(settings.HIT_EXPIRATION)) < now:
-            self.delete()
-
 
 
