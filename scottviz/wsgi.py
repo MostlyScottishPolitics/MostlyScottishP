bc8ff660fec77def974e96436ed552cde6ac9f8a
LauraVoinea <1006414v@student.glasgow.ac.uk>
2014-12-14 18:58:07 +0000

    wsgi change

--- scottviz/wsgi.py
+++ scottviz/wsgi.py
@@ -15,7 +15,9 @@ framework.
 """
 import os
 import sys
-
+file_dir = os.path.realpath(os.path.dirname(__file__))
+sys.path.insert(0, os.path.normpath(os.path.join(file_dir, '..')))
+sys.path.insert(0, os.path.normpath(os.path.join(file_dir)))
 # We defer to a DJANGO_SETTINGS_MODULE already in the environment. This breaks
 # if running multiple sites in the same mod_wsgi process. To fix this, use
 # mod_wsgi daemon mode with each site in its own daemon process, or use
@@ -25,9 +27,7 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "scottviz.settings")
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
 # setting points here.
-file_dir = os.path.realpath(os.path.dirname(__file__))
-sys.path.insert(0, os.path.normpath(os.path.join(file_dir, '..')))
-sys.path.insert(0, os.path.normpath(os.path.join(file_dir)))
+
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
