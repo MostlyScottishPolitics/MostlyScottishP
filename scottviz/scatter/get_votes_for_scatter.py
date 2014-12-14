57cbe21c9e05c63adf998b71c8c0b2965d728149
Cristinapisi <2168879m@student.gla.ac.uk>
2014-12-14 15:32:50 +0000

    more refactoring

--- scottviz/scatter/get_votes_for_scatter.py
+++ scottviz/scatter/get_votes_for_scatter.py
@@ -2,4 +2,20 @@ __author__ = '2168879m'
 
 
 def get_votes_for_scatter(parties,topics):
-    
+
+    any_parties = len(parties)
+    any_topics = len(topics)
+
+    if any_parties and any_topics:
+        # filter by both
+
+    elif any_parties:
+        # filter by party
+
+    elif any_topics:
+        #filter by topics
+
+    else:
+        # no filter
+
+    return votes
